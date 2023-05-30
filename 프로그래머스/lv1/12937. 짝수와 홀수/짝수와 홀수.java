efe8de044d4b0d1dd41ad9ce34c1b511e25d7995
Imji <53859836+Jeonminji@users.noreply.github.com>
2023-05-30 10:44:59 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -1,14 +1,13 @@
 class Solution {
     public String solution(int num) {
-     
+        String answer = "";
         
-        
-        String answer = "";  
-        if(num % 2 == 0) {
+        if (num % 2 == 0) {
             answer = "Even";
         } else {
             answer = "Odd";
         }
+        
         return answer;
     }
 }
